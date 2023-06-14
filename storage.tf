df4e6c73802468d176a7ff28e60cac129a4aa99e
Douglas Francis <douglas@poshops.io>
2023-06-14 15:52:08 -0400

    fixing diag SA name

--- storage.tf
+++ storage.tf
@@ -54,50 +54,13 @@ resource "azurerm_monitor_diagnostic_setting" "sa_blob_diag" {
   }
 }
 
-# File storage logging
+# SA storage logging
 resource "azurerm_monitor_diagnostic_setting" "sa_diag" {
   count                      = var.resource_type == "sa" ? 1 : 0
   log_analytics_workspace_id = var.diag_log_analytics_id
   name                       = "storage_securityLogs"
   target_resource_id         = var.resource_id
 
-  enabled_log {
-    category = "StorageDelete"
-
-
-    retention_policy {
-      days    = 365
-      enabled = true
-    }
-  }
-  enabled_log {
-    category = "StorageRead"
-
-
-    retention_policy {
-      days    = 365
-      enabled = true
-    }
-  }
-  enabled_log {
-    category = "StorageWrite"
-
-
-    retention_policy {
-      days    = 365
-      enabled = true
-    }
-  }
-
-  metric {
-    category = "Capacity"
-
-
-    retention_policy {
-      days    = 365
-      enabled = false
-    }
-  }
   metric {
     category = "Transaction"
     enabled  = false
@@ -171,6 +134,62 @@ resource "azurerm_monitor_diagnostic_setting" "sa_table_diag" {
   log_analytics_workspace_id = var.diag_log_analytics_id
   name                       = "storage_securityLogs"
   target_resource_id         = "${var.resource_id}/tableServices/default"
+
+  enabled_log {
+    category = "StorageDelete"
+
+
+    retention_policy {
+      days    = 365
+      enabled = true
+    }
+  }
+  enabled_log {
+    category = "StorageRead"
+
+
+    retention_policy {
+      days    = 365
+      enabled = true
+    }
+  }
+  enabled_log {
+    category = "StorageWrite"
+
+
+    retention_policy {
+      days    = 365
+      enabled = true
+    }
+  }
+
+  metric {
+    category = "Capacity"
+
+
+    retention_policy {
+      days    = 365
+      enabled = false
+    }
+  }
+  metric {
+    category = "Transaction"
+    enabled  = false
+
+    retention_policy {
+      days    = 365
+      enabled = true
+    }
+  }
+}
+
+# File Logging
+
+resource "azurerm_monitor_diagnostic_setting" "sa_table_diag" {
+  count                      = var.resource_type == "sa" ? 1 : 0
+  log_analytics_workspace_id = var.diag_log_analytics_id
+  name                       = "storage_securityLogs"
+  target_resource_id         = "${var.resource_id}/fileServices/default"
 
   enabled_log {
     category = "StorageDelete"
