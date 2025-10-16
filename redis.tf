ac4550f7eb9f8bcc7c62cf531c7bdaea01a5ab1a
Kinsey Matthews <100802530+KMatt11@users.noreply.github.com>
2025-10-16 15:24:55 -0400

    fix(diagnostics): update redis.tf

--- redis.tf
+++ redis.tf
@@ -1,14 +1,26 @@
 resource "azurerm_monitor_diagnostic_setting" "rdc_diag" {
   count                      = var.resource_type == "rdc" ? 1 : 0
   log_analytics_workspace_id = var.diag_log_analytics_id
-  name                       = "redis_securityLogs"
+  name                       = "redis_diagnosticLogs"
   target_resource_id         = var.resource_id
 
-  enabled_log {
+  log {
     category = "ConnectedClientList"
+    enabled  = true
+
+    retention_policy {
+      enabled = false
+      days    = 0
+    }
   }
 
   metric {
     category = "AllMetrics"
+    enabled  = true
+
+    retention_policy {
+      enabled = false
+      days    = 0
+    }
   }
 }
