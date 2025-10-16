312c5e6cb8b2000d4e340608dced902a9fae9898
Kinsey Matthews <100802530+KMatt11@users.noreply.github.com>
2025-10-16 13:22:12 -0400

    fix(diagnostics): update mysql.tf

--- mysql.tf
+++ mysql.tf
@@ -1,17 +1,36 @@
 resource "azurerm_monitor_diagnostic_setting" "msql_diag" {
   count                      = var.resource_type == "msql" ? 1 : 0
   log_analytics_workspace_id = var.diag_log_analytics_id
-  name                       = "mysql_securityLogs"
+  name                       = "mysql_diagnosticLogs"
   target_resource_id         = var.resource_id
 
-  enabled_log {
+  log {
     category = "MySqlAuditLogs"
+    enabled  = true
+
+    retention_policy {
+      enabled = false
+      days    = 0
+    }
   }
-  enabled_log {
+
+  log {
     category = "MySqlSlowLogs"
+    enabled  = true
+
+    retention_policy {
+      enabled = false
+      days    = 0
+    }
   }
 
   metric {
     category = "AllMetrics"
+    enabled  = true
+
+    retention_policy {
+      enabled = false
+      days    = 0
+    }
   }
 }
